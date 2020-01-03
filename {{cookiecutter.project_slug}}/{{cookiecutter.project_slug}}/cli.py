44189292b4594a8de3d7298dab298e83e186ad43
romnnn <roman.dahm@student.hpi.uni-potsdam.de>
2020-01-03 22:56:38 +0100

    Formatting

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -10,10 +10,7 @@ import click
 @click.command()
 def main(args: typing.Optional[str] = None) -> int:
     """Console script for {{cookiecutter.project_slug}}."""
-    click.echo(
-        "Replace this message by putting your code into "
-        "{{cookiecutter.project_slug}}.cli.main"
-    )
+    click.echo("Replace this message by putting your code into {{cookiecutter.project_slug}}.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
     return 0
 
